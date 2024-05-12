dcba908035381960af144472adedc0cd9d185ba1
ArisuSak <120944868+ArisuSak@users.noreply.github.com>
2024-05-12 15:22:52 +0700

    feat : initial layout

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+export default function HomePage() {
+  return (
+    <Box>
+      <Typography variant="h1" sx={{ color: "black" }}>
+        homepage
+      </Typography>
+    </Box>
+  );
+}
